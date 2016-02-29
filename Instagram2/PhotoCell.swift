12eb47f33f8f957ce9464416ed9abf1441ae021c
dlangner5023 <dlangner@uw.edu>
2016-02-28 23:00:36 -0800

    added all required and optional features

---
+++ Instagram2/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Instagram2
+//
+//  Created by Dustin Langner on 2/27/16.
+//  Copyright © 2016 Dustin Langner. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Instagram2/UserMedia.swift
@@ -0,0 +1,13 @@
+//
+//  UserMedia.swift
+//  Instagram2
+//
+//  Created by Dustin Langner on 2/28/16.
+//  Copyright © 2016 Dustin Langner. All rights reserved.
+//
+
+import UIKit
+
+class UserMedia: NSObject {
+
+}
